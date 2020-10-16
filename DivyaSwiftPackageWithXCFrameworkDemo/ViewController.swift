0cd62356b7a1d211c5b0ba02d9ba9fbbb476c3ff
Divya Suthar <divyasuthar1605@gmail.com>
2020-10-16 18:16:41 +0530

    Added swift package manager that uses xcframework

--- DivyaSwiftPackageWithXCFrameworkDemo/ViewController.swift
+++ DivyaSwiftPackageWithXCFrameworkDemo/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import DivyaSwiftFramework
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var container: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func clickHere(_ sender: UIButton) {
+    let hello = Hello()
+    hello.sayHello(to: self, message: "From XCFramewok Demo")
+  }
+  
 }
 
